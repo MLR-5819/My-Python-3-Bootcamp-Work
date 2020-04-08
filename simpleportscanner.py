a46519fa6931db9f0aaae5f3469f7e5cd8057d0d
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-04-08 12:01:36 -0500

    Create simpleportscanner.py

---
+++ simpleportscanner.py
@@ -0,0 +1,33 @@
+#Simple Port Scanner
+import socket
+
+#global variable - s=creates a stream socket IP4/TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#functions
+def portscan(port):
+	try:
+		s.connect((target,port))
+	except: 
+		return False
+	else:
+		return True
+
+#Main code
+print("\n" * 50)
+print(" __________________________________________")
+print("|                                          |")
+print("|           Simple Port Scanner            |")
+print("|__________________________________________|\n")
+#input validation needed
+target = input("Please enter IP address or Website to scan:\n\t> ")
+portr1 = input("Please enter port range to scan (1-65535): \n\tStart Port> ")
+portr2 = input("\tEnd Port> ")
+
+print("\nScan initiated on Target:\n" + target + ", on ports: "
+ + portr1 + "-" + portr2)
+
+for port in range(int(portr1),(int(portr2)+1)):
+	print("Scanning port "+ str(port) +"...")
+	if portscan(port):
+		print("Port "+ str(x) +": Open")

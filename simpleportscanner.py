116490952cda06545d0af4b9c0fe776a9ba1d271
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-04-08 14:55:45 -0500

    Update simpleportscanner.py

--- simpleportscanner.py
+++ simpleportscanner.py
@@ -1,10 +1,28 @@
 #Simple Port Scanner
 import socket
+import ipinfo
 
 #global variable - s=creates a stream socket IP4/TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #functions
+def iplookup():
+	try:
+		#ipinfo ip lookup
+		access_token = '123456789abc'
+		handler = ipinfo.getHandler(access_token)
+		ip_address = target
+		details = handler.getDetails(ip_address)
+		
+		#details wanted
+		ip_city = details.city
+		ip_country = details.country_name
+		ip_hostname = details.hostname
+
+		# print/confirm scan?
+	except:
+		print("Unable to lookup that IP address.")
+
 def portscan(port):
 	try:
 		s.connect((target,port))
@@ -20,10 +38,11 @@ print("|                                          |")
 print("|           Simple Port Scanner            |")
 print("|__________________________________________|\n")
 #input validation needed
-target = input("Please enter IP address or Website to scan:\n\t> ")
+target = input("Please enter IP address to scan (1.1.1.1):\n\t> ")
 portr1 = input("Please enter port range to scan (1-65535): \n\tStart Port> ")
 portr2 = input("\tEnd Port> ")
-
+iplookup()
+#add in confirm scan
 print("\nScan initiated on Target:\n" + target + ", on ports: "
  + portr1 + "-" + portr2)
 
